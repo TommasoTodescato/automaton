bbebc9bc4bacf9fabf0a46e7715e1086720f1cb6
tommasotode <tommasotodescato@gmail.com>
2024-07-09 18:29:41 +0200

    game of life works

--- include/conway/conway.go
+++ include/conway/conway.go
@@ -13,14 +13,70 @@ func fieldCopy(field [][]bool) [][]bool {
 	return cp
 }
 
-func GameOfLifeIteration(field [][]bool) {
+func countNeighbours(field [][]bool, x, y int) int {
+	count := 0
+
+	if y+1 >= len(field) || y-1 < 0 {
+		return -1
+	}
+	if x+1 >= len(field[0]) || x-1 < 0 {
+		return -1
+	}
+
+	if field[y+1][x] {
+		count++
+	}
+	if field[y+1][x+1] {
+		count++
+	}
+	if field[y+1][x-1] {
+		count++
+	}
+	if field[y][x+1] {
+		count++
+	}
+	if field[y][x-1] {
+		count++
+	}
+	if field[y-1][x] {
+		count++
+	}
+	if field[y-1][x+1] {
+		count++
+	}
+	if field[y-1][x-1] {
+		count++
+	}
+
+	return count
+}
+
+func LifeGeneration(field [][]bool) {
 	cp := fieldCopy(field)
+
+	/*
+	   Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+	   Any live cell with two or three live neighbours lives on to the next generation.
+	   Any live cell with more than three live neighbours dies, as if by overpopulation.
+	   Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+	*/
 
 	for y := range cp {
 		for x := range cp[y] {
+			neighbours := countNeighbours(cp, x, y)
+
+			if neighbours == -1 {
+				continue
+			}
+
 			if cp[y][x] {
-				field[y][x+1] = true
-				field[y][x] = false
+				if neighbours < 2 || neighbours > 3 {
+					field[y][x] = false
+				}
+			} else {
+				if neighbours == 3 {
+					field[y][x] = true
+				}
 			}
 		}
 	}
--- main.go
+++ main.go
@@ -7,21 +7,25 @@ import (
 )
 
 func main() {
-	dy, dx := 10, 10
+	dy, dx := 30, 30
 
 	f := make([][]bool, dy)
 	for i := range f {
 		f[i] = make([]bool, dx)
 	}
 
-	f[0][0] = true
-	f[2][0] = true
+	f[2][3] = true
+	f[3][4] = true
 
-	for i := 0; i < dx-1; i++ {
+	f[4][2] = true
+	f[4][3] = true
+	f[4][4] = true
+
+	for i := 0; i < 100; i++ {
 		io.PrintField(f)
 
-		conway.GameOfLifeIteration(f)
-		time.Sleep(1 * time.Second)
+		conway.LifeGeneration(f)
+		time.Sleep(500 * time.Millisecond)
 
 		io.PrintField(f)
 	}
